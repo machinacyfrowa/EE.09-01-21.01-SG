efe21d7dffed2a0b9a39b7f3339a2bf71b056e61
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2021-05-12 16:31:23 +0200

    HTML+CSS

---
+++ 00000000000/futbol.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rozgrywki futbolowe</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+    <header id="baner">
+        <h2>Światowe rozgrywki piłkarskie</h2>
+        <img src="obraz1.jpg" alt="boisko">
+    </header>
+    <section id="mecze">
+        <div id="mecz">skrypt1</div>
+    </section>
+    <main>
+        <h2>Reprezentacja Polski</h2>
+    </main>
+    <div id="lewy">
+        <p>Podaj pozycje zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy):</p>
+        <form action="futbol.php" method="post">
+            <input type="number" name="" id="">
+            <button type="submit">Sprawdź</button>
+        </form>
+        <ul>
+            <li>skrypt2</li>
+        </ul>
+    </div>
+    <div id="prawy">
+        <img src="zad1.png" alt="piłkarz">
+        <p>Autor: 00000000000</p>
+    </div>
+</body>
+
+</html>
